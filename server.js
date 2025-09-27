1c0b4c169b161703d716b60715c8daa602ec8853
Alena Sharai <exultantislupus@gmail.com>
2025-09-27 21:52:03 +0100

    chores: install nodemon

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const { createServer } = require('http');
+const { parse } = require('path');
 
 const server = createServer((req, res) => {
-  res.end('Hello world');
+  if (req.url === '/') {
+    res.end('Hello world');
+  } else if (req.url === '/test') {
+    res.end('TEST');
+  } else {
+    res.end('The URL cannot be found');
+  }
 });
 
 server.listen(8000, () => console.log('Listening for requests on port 8000'));
