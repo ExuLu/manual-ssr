f8788bfed308639b4e6d0e3280839896a41383a3
Alena Sharai <exultantislupus@gmail.com>
2025-09-27 22:59:44 +0100

    feat: add js file to browser

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ function MenuItem({ pizza }) {
 }
 
 const htmlTemplate = readFileSync('./index.html', 'utf-8');
+const clientJS = readFileSync('./client.js', 'utf-8');
 
 const server = createServer((req, res) => {
   const pathName = parse(req.url, true).pathname;
@@ -76,8 +77,11 @@ const server = createServer((req, res) => {
       'Content-type': 'text/html',
     });
     res.end(html);
-  } else if (pathName === '/test') {
-    res.end('TEST');
+  } else if (pathName === '/client.js') {
+    res.writeHead(200, {
+      'Content-type': 'application/javascript',
+    });
+    res.end(clientJS);
   } else {
     res.end('The URL cannot be found');
   }
